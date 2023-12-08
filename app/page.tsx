a24c790addee131e66fdda6de02228db91bf507c
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2023-12-07 18:09:17 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Banner from "@/components/Banner";
+import MidBanner from "@/components/MidBanner";
+
+export default function Home() {
+  return (
+    <main>
+      <Banner />
+      <MidBanner/>
+    </main>
+  )
+}
