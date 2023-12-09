a0830e01d4598e57d423e5d77dda1608c4d36af8
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2023-12-08 23:40:39 -0700

    Add files via upload

---
+++ constants/data.ts
@@ -0,0 +1,160 @@
+export const bannerData=[
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+    {
+        _id: 1,
+        title: "Ultrasonic Diffuser",
+        isNew: true,
+        oldPrice: "$45",
+        price: "$32",
+        description:
+        "Fan-Favorite Aromatherapy Diffuser - Loved worldwide by top editors and featured in print, TV, and global media. This device features separate diffuser and LED light functions, to help customize the vibe in any room. Turn your home or office into a serene oasis, elevate your mood, and enhance your overall well-being. Its sleek, modern design fits seamlessly into any decor, making a stylish addition to any room",
+        category: "Aromatherapy",
+        image:
+        "https://m.media-amazon.com/images/I/716g0ORxcVL._AC_SL1500_.jpg",
+        rating: 4.6,
+    },
+
+]
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+export const MidBannerData = [
+    {
+        _id:1,
+        title: "candle bowl",
+        isNew: true,
+        oldPrice: "200",
+        image: "https://m.media-amazon.com/images/I/51AAAyErvIL.jpg"
+    },
+    {
+        id:2,
+        title: "candle bowl",
+        isNew: true,
+        oldPrice: "200",
+        image: "https://cdn.notonthehighstreet.com/fs/cb/3f/4c82-cbd0-4cee-9c64-a622c03292f8/original_scented-glass-candles-with-petals-20-hour-burn-time.jpg"
+    },
+    {
+        id:3,
+        title: "candle bowl",
+        isNew: true,
+        oldPrice: "200",
+        image: "https://i.pinimg.com/originals/ce/a0/7e/cea07e50dbf49a7e8225281c8ea4ac72.jpg"
+    }
+]
